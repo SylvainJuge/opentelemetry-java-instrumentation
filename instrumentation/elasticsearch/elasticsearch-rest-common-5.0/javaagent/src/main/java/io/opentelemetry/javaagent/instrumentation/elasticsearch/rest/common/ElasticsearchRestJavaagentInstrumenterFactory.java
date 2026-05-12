25a11eea1bd7b053319b89451ce2e6e98ca3eb49
Sylvain Juge <763082+SylvainJuge@users.noreply.github.com>
2026-05-12 14:05:48 +0200

    elasticsearch

--- instrumentation/elasticsearch/elasticsearch-rest-common-5.0/javaagent/src/main/java/io/opentelemetry/javaagent/instrumentation/elasticsearch/rest/common/ElasticsearchRestJavaagentInstrumenterFactory.java
+++ instrumentation/elasticsearch/elasticsearch-rest-common-5.0/javaagent/src/main/java/io/opentelemetry/javaagent/instrumentation/elasticsearch/rest/common/ElasticsearchRestJavaagentInstrumenterFactory.java
@@ -3,7 +3,7 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-package io.opentelemetry.javaagent.instrumentation.elasticsearch.rest;
+package io.opentelemetry.javaagent.instrumentation.elasticsearch.rest.common;
 
 import static java.util.Collections.emptyList;
 
